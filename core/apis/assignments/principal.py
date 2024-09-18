872e1f9f441e1daeb2c55525f7e0ef8a93af5bef
eniya-r <eng21cs0127eniya@gmail.com>
2024-09-18 15:53:31 +0530

    Update principal.py

--- core/apis/assignments/principal.py
+++ core/apis/assignments/principal.py
@@ -5,3 +5,11 @@ def list_submitted_assignments(p):
     assignments = Assignment.get_all_submitted_assignments()
     assignments_dump = AssignmentSchema().dump(assignments, many=True)
     return APIResponse.respond(data=assignments_dump)
+    @principal_bp.route('/teachers', methods=['GET'], strict_slashes=False)
+@decorators.authenticate_principal
+def list_teachers(p):
+    """List all teachers"""
+    teachers = Teacher.get_all_teachers()  
+    teachers_dump = TeacherSchema().dump(teachers, many=True) 
+    return APIResponse.respond(data=teachers_dump)
+
